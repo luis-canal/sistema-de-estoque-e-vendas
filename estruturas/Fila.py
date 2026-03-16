41b3d2723c539c7923327da53a13872f1113ff96
Guilherme Menzen <menzenguilherme@gmail.com>
2026-03-16 20:39:43 -0300

    Terminando fila

--- estruturas/Fila.py
+++ estruturas/Fila.py
@@ -10,3 +10,17 @@ class Fila:
         if self.is_empty(): # a verdade para desinfileirar, é que precisar ter algum dado na fila
             print("A fila está vazia")
             return
+        
+        self._itens.pop(0)
+
+    def proximo_a_sair_da_fila(self):
+        print(self._itens[0])
+    
+    def is_empty(self):
+        return len(self._itens) == 0
+    
+    def __len__(self):
+        return len(self._itens)
+
+    def __str__(self): # representacao da fila como uma lista
+        return str(self._itens)
