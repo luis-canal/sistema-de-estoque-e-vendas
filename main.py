035160fa8d588b7a4b632df27b07e08e749db0a3
Kaiki Pauletto <kaikipauletto@hotmail.com>
2026-03-17 11:36:49 -0300

    Implementação do inicio do  menu funcionando

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 from sistema.sistema_estoque import *
+
+def menu():
+    while True:
+        print("\n===== MENU =====")
+        print("1 - Cadastrar cliente")
+        print("2 - Listar clientes")
+        print("3 - Cadastrar produto")
+        print("4 - Listar produtos")
+        print("0 - Sair")
+
+        opcao = input("Escolha: ")
+
+        if opcao == "1":
+            cadastrarCliente()
+
+        elif opcao == "2":
+            listarClientes()
+
+        elif opcao == "3":
+            cadastrarProduto()
+
+        elif opcao == "4":
+            listarProdutos()
+
+        elif opcao == "0":
+            break
+
+        else:
+            print("Opção inválida")
+            return False
+        
+menu()
