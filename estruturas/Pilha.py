ec9550b548c0bd68f77e2c85438a466d1fc7d9fc
Guilherme Menzen <menzenguilherme@gmail.com>
2026-03-16 21:33:53 -0300

    Implementando Pilha

---
+++ estruturas/Pilha.py
@@ -0,0 +1,28 @@
+class Pilha:
+
+    def __init__(self):
+        self._itens = []
+
+    def push(self, item):
+        self._itens.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Não é possível remover dados da pilha, pois a pilha está vazia")
+
+        return self._itens.pop()
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Não é possível retornar o topo da pilha, pois a pilha está vazia")
+
+        return self._itens[-1]
+
+    def is_empty(self):
+        return len(self._itens) == 0
+
+    def __str__(self):
+        return str(self._itens)
+    
+    def __len__(self):
+        return len(self._itens)
