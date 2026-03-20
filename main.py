46e0ba4b751d7986c0e04aceb038de4cfc62ffdb
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-03-20 19:35:24 -0300

    atualizando main.py, menu e suas opções

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 from sistema.sistema_estoque import SistemaEstoque
-import os
+import os, time
 
 def menu():
     sistema = SistemaEstoque()
 
     while True:
-        print("\n===== MENU =====")
+# ===== MENU ESTOQUE =====
+# 1 - Cadastrar cliente (certo)
+# 2 - Listar clientes (certo)
+# 3 - Cadastrar produto (certo)
+# 4 - Listar produtos (certo)
+# remover cliente NOVO
+# remover produto NOVO
+# 5 - Pesquisar produto 
+# 6 - Realizar venda
+# 7 - Ver fila de vendas
+# 8 - Desfazer última operação (certo)
+# 9 - Exibir valor total do estoque
+# 10 - Exibir valor total de vendas
+# 11 - Exibir clientes e valores gastos
+# 12 - Sair (certo)
+# ========================
+        print("\n===== MENU ESTOQUE =====")
         print("1 - Cadastrar cliente")
         print("2 - Listar clientes")
         print("3 - Cadastrar produto")
         print("4 - Listar produtos")
-        print("5 - Desfazer operação")
+        print("5 - Desfazer última operação")
         print("6 - Remover cliente(id)")
         print("7 - Remover produto(id)")
         print("0 - Sair")
+        print("========================")
 
         opcao = input("Escolha: ")
 
@@ -39,10 +56,12 @@ def menu():
             sistema.remover_produto()
 
         elif opcao == "0":
+            print("Saindo...")
+            time.sleep(2)
             break
 
         else:
-            print("Opção inválida")
+            print("Opção inválida, tente novamente")
 
 
 menu()
